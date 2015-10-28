556ab8ce2c4dbb3dc95b85f4354395fd251a79ca
Celine Langlade <coklicote@hotmail.fr>
2015-10-28 10:21:22 +0100

    ajout doctrine fixture et bouton enregistrer et css

---
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,24 @@
+<?php
+// src/AppBundle/DataFixtures/ORM/LoadUserData.php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\User;
+
+class LoadUserData implements FixtureInterface
+{
+    /**
+    * {@inheritDoc}
+    */
+    public function load(ObjectManager $manager)
+    {
+        $userAdmin = new User();
+        $userAdmin->setUsername('admin');
+        $userAdmin->setPassword('test');
+
+        $manager->persist($userAdmin);
+        $manager->flush();
+    }
+}
